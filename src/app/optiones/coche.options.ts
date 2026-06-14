5c3a0c40f446c124db3e52c228110461ef49b5af
Joao <goku_bh@hotmail.com>
2026-06-13 22:27:30 -0500

    Agrego seguridad opciones y modelos

---
+++ src/app/optiones/coche.options.ts
@@ -0,0 +1,16 @@
+export const COMBUSTIBLES_COCHE = [
+  'GNV',
+  'GLP',
+  'GNL',
+  'Gasolina (Nafta)',
+  'Diesel'
+];
+
+export const MARCAS_COCHE = [
+  'Toyota',
+  'Hyundai',
+  'Kia',
+  'Nissan',
+  'Chevrolet',
+  'Ford'
+];
